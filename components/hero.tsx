571fdfe1e33dc19316811ac4f9587908d31e79a7
bksinn <bksinn@gmail.com>
2025-06-28 16:45:01 -0400

    Hero section adjustments

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,18 +4,18 @@ import Image from "next/image"
 
 export default function Hero() {
   return (
-    <section className="container flex min-h-[calc(100vh-3.5rem)] max-w-screen-2xl flex-col items-center justify-center py-12 text-center">
+    <section className="container -mt-10 flex min-h-[calc(100vh-3.5rem)] max-w-screen-2xl flex-col items-center justify-center text-center">
       <div className="space-y-6">
         <img
           src="/images/SVG_Vector_Files/Transparent_Logo.svg"
           alt="Bravax Logo"
-          className="mx-auto h-80"
+          className="mx-auto md:h-[30rem] -my-10"
         />
-        <h2 className="bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl">
+        <h2 className="bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-3xl lg:text-5xl font-bold tracking-tight text-transparent">
           Real-Time Payment Trust, Before the Money Moves
         </h2>
         <p className="mx-auto max-w-[50rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-          Invoice fraud doesn’t wait — and neither should your defenses. Bravax sits between email and AP workflows to verify financial trust <strong>before approvals flow downstream.</strong>  
+          Invoice fraud doesn't wait — and neither should your defenses. Bravax sits between email and AP workflows to verify financial trust <strong>before approvals flow downstream.</strong>  
         </p>
         <p className="mx-auto max-w-[50rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
         No install. No disruption. Just quiet protection at the moment it matters most.
